b01b9d6246b0d5477e5cd931b8201b933768bf7c
Tomas Madajevas <tomas.madajevas@live.com>
2021-08-25 06:24:28 +0300

    Add project files.

---
+++ HttpToGrpcProxy/Controllers/EverythingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpToGrpcProxy.Controllers;
+[ApiController]
+public class EverythingController : ControllerBase
+{
+    private readonly ILogger<EverythingController> _logger;
+
+    public EverythingController(ILogger<EverythingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Route("{**route}")]
+    [HttpGet, HttpPost, HttpDelete, HttpHead, HttpOptions, HttpPatch, HttpPut]
+    public IActionResult Everything(string route)
+    {
+        return Ok(new { Route = route, Method = Request.Method });
+    }
+}
---
+++ HttpToGrpcProxy/Program.cs
@@ -0,0 +1,22 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
