0c2e4283d8819a774d69b3578002b4694d8c7fd2
Tomas Madajevas <tomas.madajevas@live.com>
2021-08-25 22:02:38 +0300

    both grpc and http started on same host

--- HttpToGrpcProxy/Program.cs
+++ HttpToGrpcProxy/Program.cs
@@ -1,28 +1,50 @@
 
 using HttpToGrpcProxy.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-// Add services to the container.
-builder.Services.AddSingleton<ProxyService>();
-builder.Services.AddGrpc();
+using System.Net;
 
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (builder.Environment.IsDevelopment())
+public class Program
 {
-    app.UseDeveloperExceptionPage();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.WebHost.UseKestrel()
+            .ConfigureKestrel((context, options) =>
+            {
+                // grpc
+                options.Listen(IPAddress.Any, 6000, listenOptions => listenOptions.Protocols = HttpProtocols.Http2);
+                // http/rest
+                options.Listen(IPAddress.Any, 5000, listenOptions => listenOptions.Protocols = HttpProtocols.Http1);
+            });
+
+        // Add services to the container.
+        builder.Services.AddSingleton<ProxyService>();
+        builder.Services.AddGrpc();
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (builder.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // app.UseHttpsRedirection();
+
+        // app.UseAuthorization();
+
+        app.MapControllers();
+        app.UseRouting();
+
+
+
+        app.MapGrpcService<ProxyService>().RequireHost($"*:6000");
+
+        app.Run();
+    }
 }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
-app.MapControllers();
-app.UseRouting();
-app.MapGrpcService<ProxyService>();
-
-app.Run();
