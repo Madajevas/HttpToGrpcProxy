17b7b5bd77a76bf8642891b982923a5594779f4e
Tomas Madajevas <tomas.madajevas@live.com>
2021-08-25 22:52:25 +0300

    initial integratin test. just server is started

---
+++ HttpToGrpcProxy.Tests/Integration.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpToGrpcProxy.Tests
+{
+    public class Integration
+    {
+        [SetUp]
+        public async Task SetUp()
+        {
+            Program.Main(Array.Empty<string>());
+            await Task.Delay(2000);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Program.app.StopAsync();
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
