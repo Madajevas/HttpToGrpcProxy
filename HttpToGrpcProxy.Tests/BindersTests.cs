f46a075bfa8883ccb9ea90a39c9201beb5112372
Tomas Madajevas <tomas.madajevas@live.com>
2021-09-09 10:57:32 +0300

    binding from form implemented

--- HttpToGrpcProxy.Tests/BindersTests.cs
+++ HttpToGrpcProxy.Tests/BindersTests.cs
@@ -8,7 +8,7 @@ namespace HttpToGrpcProxy.Tests
 {
     public class BindersTests : IntegrationTestsBase
     {
-        class JsonBinding
+        class Model
         {
             public long Id { get;set; }
             public string Name {  get; set; }
@@ -17,21 +17,39 @@ namespace HttpToGrpcProxy.Tests
         [Test]
         public async Task CanBindRequestBodyToClass()
         {
-            var model = new JsonBinding { Id = 1, Name = "name" };
+            var model = new Model { Id = 1, Name = "name" };
             var json = JsonSerializer.Serialize(model);
 
             var request = new RestRequest("/anything/json");
             request.AddParameter("application/json", json, ParameterType.RequestBody);
-            var resultPromise = HttpClient.PostAsync<string>(request);
+            var _ = HttpClient.PostAsync<string>(request);
 
             using var requestContext = await Proxy.InterceptRequest("anything/json");
 
             Assert.That(requestContext.ContentType, Is.EqualTo("application/json"));
 
-            var receivedModel = requestContext.BindJson<JsonBinding>();
+            var receivedModel = requestContext.BindJson<Model>();
 
             Assert.That(receivedModel.Id, Is.EqualTo(1));
             Assert.That(receivedModel.Name, Is.EqualTo("name"));
+        }
+
+        [Test]
+        public async Task CanBindFormToClass()
+        {
+            var request = new RestRequest("anything/form");
+            request.AddParameter("id", 42);
+            request.AddParameter("name", "test");
+            var _ = HttpClient.PostAsync<string>(request);
+
+            using var requestContext = await Proxy.InterceptRequest("anything/form");
+
+            Assert.That(requestContext.ContentType, Is.EqualTo("application/x-www-form-urlencoded"));
+
+            var receivedModel = requestContext.BindForm<Model>();
+
+            Assert.That(receivedModel.Id, Is.EqualTo(42));
+            Assert.That(receivedModel.Name, Is.EqualTo("test"));
         }
     }
 }
--- ProxyInterceptorTestsClient/BindingExtensions.cs
+++ ProxyInterceptorTestsClient/BindingExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Web;
 
 namespace ProxyInterceptorTestsClient
 {
@@ -7,6 +10,27 @@ namespace ProxyInterceptorTestsClient
         public static T BindJson<T>(this RequestContext requestContext)
         {
             return JsonSerializer.Deserialize<T>(requestContext.Body);
+        }
+
+        public static T BindForm<T>(this RequestContext requestContext) where T : new()
+        {
+            var properties = typeof(T).GetProperties().ToDictionary(p => p.Name.ToLower(), p => p);
+            var form = HttpUtility.ParseQueryString(requestContext.Body);
+
+            var instance = new T();
+
+            foreach (var key in form.AllKeys)
+            {
+                if (!properties.TryGetValue(key.ToLower(), out var property))
+                {
+                    continue;
+                }
+
+                var convertedValue = Convert.ChangeType(form[key], property.PropertyType);
+                property.SetValue(instance, convertedValue);
+            }
+
+            return instance;
         }
     }
 }
