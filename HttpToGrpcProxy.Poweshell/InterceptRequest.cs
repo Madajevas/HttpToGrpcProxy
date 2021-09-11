c092b26461ff8bb1d6126a3cba10179aa1d5bd43
Tomas Madajevas <tomas.madajevas@live.com>
2021-09-11 19:50:38 +0300

    initial powershell client project added

---
+++ HttpToGrpcProxy.Poweshell/InterceptRequest.cs
@@ -0,0 +1,24 @@
+﻿using ProxyInterceptorTestsClient;
+
+using System;
+using System.Management.Automation;
+
+namespace HttpToGrpcProxy.Poweshell
+{
+    [Cmdlet("Init", "Client")]
+    [OutputType(typeof(Client))]
+    public class InterceptRequest : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Uri ProxyAddress { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var client = new Client(ProxyAddress);
+
+            WriteObject(client);
+        }
+    }
+}
