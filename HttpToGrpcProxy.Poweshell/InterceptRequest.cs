98164d48c9f853e44fbf551af36b7f30fc5e4cf0
Tomas Madajevas <tomas.madajevas@live.com>
2021-09-11 20:02:02 +0300

    intercept request implemented (not tested)

--- HttpToGrpcProxy.Poweshell/InterceptRequest.cs
+++ HttpToGrpcProxy.Poweshell/InterceptRequest.cs
@@ -2,23 +2,41 @@
 
 using System;
 using System.Management.Automation;
+using System.Threading.Tasks;
 
 namespace HttpToGrpcProxy.Poweshell
 {
-    [Cmdlet("Init", "Client")]
-    [OutputType(typeof(Client))]
+    [Cmdlet("Intercept", "Request")]
+    [OutputType(typeof(RequestContext))]
     public class InterceptRequest : PSCmdlet
     {
         [Parameter(Mandatory = true)]
-        public Uri ProxyAddress { get; set; }
+        public Client ProxyClient { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string Route { get; set; }
+
+        [Parameter]
+        public TimeSpan? Timeout { get; set; }
 
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
 
-            var client = new Client(ProxyAddress);
+            Task<RequestContext> interceptTask;
+            if (Timeout.HasValue)
+            {
+                interceptTask = ProxyClient.InterceptRequest(Route, Timeout.Value);
+            }
+            else
+            {
+                interceptTask = ProxyClient.InterceptRequest(Route);
+            }
 
-            WriteObject(client);
+            // seems there is no async method, hence blocking
+            var requestContext = interceptTask.GetAwaiter().GetResult();
+
+            WriteObject(requestContext);
         }
     }
 }
