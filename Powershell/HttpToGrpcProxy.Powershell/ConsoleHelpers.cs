3796656022e287841139858e3b0baa9da0b2a84f
Tomas Madajevas <tomas.madajevas@live.com>
2021-09-18 11:09:28 +0300

    quit waiting for intercepted request if Ctrl+C is pressed

---
+++ Powershell/HttpToGrpcProxy.Powershell/ConsoleHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace HttpToGrpcProxy.Powershell
+{
+    static class ConsoleHelpers
+    {
+        public static void CancelTokenOnConsoleCancel(CancellationTokenSource cancellationTokenSource)
+        {
+            Console.CancelKeyPress += (_, __) =>
+            {
+                Console.WriteLine("Termination command received");
+                cancellationTokenSource.Cancel();
+            };
+        }
+    }
+}
--- Powershell/HttpToGrpcProxy.Powershell/InterceptRequest.cs
+++ Powershell/HttpToGrpcProxy.Powershell/InterceptRequest.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.Management.Automation;
-using System.Threading.Tasks;
+using System.Threading;
+
+using static HttpToGrpcProxy.Powershell.ConsoleHelpers;
 
 namespace HttpToGrpcProxy.Powershell
 {
@@ -23,15 +25,10 @@ namespace HttpToGrpcProxy.Powershell
         {
             base.ProcessRecord();
 
-            Task<RequestContext> interceptTask;
-            if (Timeout.HasValue)
-            {
-                interceptTask = ProxyClient.InterceptRequest(Route, Timeout.Value);
-            }
-            else
-            {
-                interceptTask = ProxyClient.InterceptRequest(Route);
-            }
+            var cancellationTokenSource = Timeout.HasValue ? new CancellationTokenSource(Timeout.Value) : new CancellationTokenSource();
+            CancelTokenOnConsoleCancel(cancellationTokenSource);
+
+            var interceptTask = ProxyClient.InterceptRequest(Route, cancellationTokenSource.Token);
 
             // seems there is no async method, hence blocking
             var requestContext = interceptTask.GetAwaiter().GetResult();
