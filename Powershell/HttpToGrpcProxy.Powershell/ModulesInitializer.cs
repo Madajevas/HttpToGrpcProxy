88a4cbc67b87e83b3818a5139e41848e295e475a
Tomas Madajevas <tomas.madajevas@live.com>
2021-09-18 11:01:47 +0300

    modules initializer added to try and load unmet dependencies

---
+++ Powershell/HttpToGrpcProxy.Powershell/ModulesInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace HttpToGrpcProxy.Powershell
+{
+    public class ModuleInitializer : IModuleAssemblyInitializer, IModuleAssemblyCleanup
+    {
+        private static readonly string s_modulePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public void OnImport()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        public void OnRemove(PSModuleInfo psModuleInfo)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+
+            return Assembly.LoadFrom(Path.Combine(s_modulePath, $"{assemblyName.Name}.dll"));
+        }
+    }
+}
